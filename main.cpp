622382cc04d2a14327a352447c782b44afe5aaa7
Miguel Lopes <miguel.lopes@miniclip.com>
2020-07-12 18:00:59 +0100

    add ray

--- main.cpp
+++ main.cpp
@@ -7,21 +7,48 @@
 //
 
 #include <iostream>
+#include "ray.h"
+
+///returns color of the background, simple gradient
+///linear blend / lerp / linear interpolation
+vec3 color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5*(unit_direction.y() + 1.0);
+    
+    //p(t) = A + t*B
+    ///Here p​ is a 3D position along a line in 3D.
+    ///A​ is the ray origin and B​ is the ray direction.
+    ///The ray parameter t is a real number.
+    
+    return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+}
+
+
 
 int main(int argc, const char * argv[]) {
     
     int nx = 200;
     int ny = 100;
     std::cout<< "P3\n" << nx << " " << ny << "\n255\n";
+    
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
+    
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
             
-            int ir = int(255.99*r);
-            int ig = int(255.99*g);
-            int ib = int(255.99*b);
+            float u = float(i) / float(nx);
+            float v = float(j) / float(ny);
+            
+            ray r(origin,
+                  lower_left_corner + u*horizontal + v*vertical);
+            vec3 col = color(r);
+            
+            int ir = int(255.99*col[0]);
+            int ig = int(255.99*col[1]);
+            int ib = int(255.99*col[2]);
             
             std::cout<< ir << " "<< ig << " " << ib << std::endl;;
         }
---
+++ ray.h
@@ -0,0 +1,22 @@
+//
+//  ray.h
+//  RayTracing
+//
+//  Created by Miguel Lopes on 12/07/2020.
+//  Copyright © 2020 Miguel Lopes. All rights reserved.
+//
+
+#include "vec3.h"
+
+class ray {
+public:
+    ray() {}
+    ray(const vec3& a, const vec3& b){ A = a; B = b;}
+    vec3 origin() const { return A;}
+    vec3 direction() const { return B;}
+    vec3 point_at_parameter(float t) const {return A + t*B;}
+    
+    
+    vec3 A;
+    vec3 B;
+};
