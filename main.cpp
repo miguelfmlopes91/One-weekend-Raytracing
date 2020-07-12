c05090e0ac85cf91e928c944486a081d240d6bb9
Miguel Lopes <miguel.lopes@miniclip.com>
2020-07-12 22:44:16 +0100

    adding a sphere

--- main.cpp
+++ main.cpp
@@ -9,11 +9,26 @@
 #include <iostream>
 #include "ray.h"
 
+
+bool hit_sphere(const vec3& center, float radius, const ray& r){
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    return (discriminant > 0); //formula resolvente de func quadrática
+}
+
 ///returns color of the background, simple gradient
 ///linear blend / lerp / linear interpolation
 ///A lerp is always of the form:
 ///blended_value = (1-t)*start_value + t*end_value, with tgoing from zero to one.
 vec3 color(const ray& r) {
+    
+    //test
+    if (hit_sphere(vec3(0,0,1), 0.5, r)) {
+        return vec3(1, 0, 0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5*(unit_direction.y() + 1.0);
     
