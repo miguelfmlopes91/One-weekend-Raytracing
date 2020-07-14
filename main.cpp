b7cfcd195905e18484f70973dbceeee26eb58c8c
Miguel Lopes <miguel.lopes@miniclip.com>
2020-07-14 23:47:36 +0100

    diffuse materials

--- main.cpp
+++ main.cpp
@@ -11,6 +11,17 @@
 #include "camera.h"
 #include <cfloat>
 
+
+vec3 random_in_unit_sphere(){
+    vec3 p;
+    
+    do {
+        p = 2.0*vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0);
+    
+    return p;
+}
+
 ///returns color of the background, simple gradient
 ///linear blend / lerp / linear interpolation
 ///A lerp is always of the form:
@@ -18,8 +29,9 @@
 vec3 color(const ray& r, hitable* world){
     hit_record rec;
     
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5 * vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+    if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * color(ray(rec.p, target - rec.p), world);
     } else {
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5*(unit_direction.y() + 1.0);
@@ -58,6 +70,8 @@ int main(int argc, const char * argv[]) {
             }
             
             col /= float(ns);
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+            
             
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
